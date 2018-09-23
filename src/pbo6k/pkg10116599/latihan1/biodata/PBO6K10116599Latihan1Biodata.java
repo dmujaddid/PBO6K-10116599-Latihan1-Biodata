66a79c230deced2b53871da6addfac9b4a81d9b5
dmujaddid <robyeka@email.unikom.ac.id>
2018-09-23 10:32:55 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan1/biodata/PBO6K10116599Latihan1Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program ini berisi program perintah menampilkan value ke layar
+ * 
+ */
+public class PBO6K10116599Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: Rizki Adam Kurniawan");
+        System.out.println("ALAMT\t\t: Jalan Antapani no 89" );
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Januari 1991");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
